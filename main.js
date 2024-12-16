615d9d03a182c3d29a041be24b9a55f19046bdb6
TestUser1938 <cumaboba988@gmail.com>
2024-12-16 15:33:31 +0200

    init

---
+++ main.js
@@ -0,0 +1,15 @@
+// Refactor following solution
+// Return an array without duplicates
+
+duplicate = (value, N) => {
+  if (N <= 0) return [];
+  else {
+    res = [];
+    for (let i = 0; i < N; i++) {
+      res[i] = value;
+    }
+    return res;
+  }
+};
+
+module.exports = duplicate;
